dac1373880b754e2eebddc16c9ac762c22e5c4cb
Denis <denisoleshenko@gmail.com>
2016-12-14 22:43:33 +0200

    Add files via upload

---
+++ oop.py
@@ -0,0 +1,108 @@
+import random, functools
+
+def food(*args):
+    def food_factory(func):
+        @functools.wraps(func)
+        def wrapper(self, unit):
+            result = type(unit) in args
+            return func(self, unit, result)
+        return wrapper
+    return food_factory
+
+class Seaweed():
+
+	def __init__(self, weight):
+		if weight > 3 or weight < 1:
+			raise ValueError("Weight must be less then 4 and more than 0")
+		self.weight = weight
+
+class Fish():
+
+	def __init__(self, weight):
+		if weight > 9 or weight < 1:
+			raise ValueError("Weight must be less then 10 and more than 0")
+		self.weight = weight
+		self.count_unit_eaten = 0
+
+	def hunt(self, aquarium):
+		i = random.randint(0, len(aquarium)-1)
+		some_unit = aquarium[i]
+		was_eat = self.eat(some_unit)
+		if was_eat:						# Если хищник съел юнита, то удаляем съеденное из аквариума
+			aquarium.pop(i)
+		#elif was_eat == False:			# если вернулась ложь, удалить рыбу из аквариума
+			#index_fish = aquarium.index(self)
+			#aquarium.pop(index_fish)
+
+
+	@food(Seaweed)
+	def eat(self, some_unit, result = False):
+		if result:
+			self.weight = self.weight + some_unit.weight
+			self.count_unit_eaten += 1
+			return True
+		#if type(some_unit) == PredatorFish:	# 	Если рыба наткнулась на хищника,съесть рыбу и вернуть ложь
+		#	some_unit.eat(self)
+		#	del self					# походу не работает
+		
+class PredatorFish(Fish):
+	DEFAULT_PREDATOR_WEIGHT = 10			# this is a constant: weight, for predator fish
+
+	def __init__(self):
+		self.weight = self.DEFAULT_PREDATOR_WEIGHT
+		self.count_fish_eaten = 0                            # count of fish eaten
+
+	def hunt(self, aquarium):
+		i = random.randint(0, len(aquarium)-1)
+		some_fish = aquarium[i]
+		was_eat = self.eat(some_fish)
+		if was_eat:						# Если хищник съел юнита, то удаляем съеденное из аквариума
+			aquarium.pop(i)
+
+	@food(Fish)
+	def eat(self, some_unit, result = False):
+		if result:
+			self.weight = self.weight + some_unit.weight
+			self.count_fish_eaten += 1
+			return True
+		#else: False
+
+class Sneil():
+
+	def __init__(self, weight):
+		if weight > 5 or weight < 1:
+			raise ValueError("Weight must be less then 6 and more than 0")
+		self.weight = weight
+		self.count_unit_eaten = 0
+
+	def hunt(self, aquarium):
+		i = random.randint(0, len(aquarium)-1)
+		some_unit = aquarium[i]
+		was_eat = self.eat(some_unit)
+		if was_eat:						# Если хищник съел юнита, то удаляем съеденное из аквариума
+			aquarium.pop(i)
+
+	@food(Seaweed)
+	def eat(self, some_unit, result = False):
+		if result:
+			self.weight = self.weight + some_unit.weight
+			self.count_unit_eaten += 1
+			return True
+
+class Aquarium:
+
+	@classmethod
+	def instance(cls):
+		if not hasattr(cls, "__instance"):
+			setattr(cls, "__instance", cls())
+		return getattr(cls, "__instance")
+
+	aquarium = []
+
+	def accept_units(self, units):	
+		self.aquarium.extend(units)	
+		if len(self.aquarium) > 100:
+			raise ValueError("Count must be less then 101")
+
+	def get_aquarium(self):
+		return self.aquarium
