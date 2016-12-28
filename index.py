64798a4f877cf4fbca7d1de91ba16641aa21f2ec
Client-Z <denisoleshenko@gmail.com>
2016-12-28 16:37:21 +0200

    restracture modules

---
+++ index.py
@@ -0,0 +1,28 @@
+from tools import *
+
+
+if __name__ == '__main__':
+
+	fish_list = create_units(Fish, random.randint(10,50), random.randint(1,9))
+	predator_list = create_units(PredatorFish, 2)
+	sneils_list = create_units(Sneil, random.randint(5,10), random.randint(1,5))
+	seaweed_list = create_units(Seaweed, random.randint(10,20), random.randint(1,3))
+
+	myAquarium = Aquarium.instance()					# Создаем пустой аквариум, через инстанс
+	myAquarium.accept_units(fish_list)					# Запускаем в него сначала обычных рыб
+	myAquarium.accept_units(predator_list)				# Теперь хищников (Все по ТЗ)
+	myAquarium.accept_units(sneils_list)				# Улитки
+	myAquarium.accept_units(seaweed_list)				# Водоросли
+	
+	print("Count units: ", len(myAquarium.aquarium))		# Посмотрим общее количество живности
+	print("Count fish: ", len(fish_list))
+	print("Count sneils: ", len(sneils_list))
+	print("Count seaweed: ", len(seaweed_list))
+	print()
+	print("*******Play*******")
+
+
+	min_count_units = len(predator_list) + len(sneils_list)
+	start_fauna(myAquarium.aquarium, min_count_units)										# Да начнется ОХОТА! ]:-)
+	out_put(predator_list, sneils_list)
+	print("Count units: ", len(myAquarium.aquarium))
--- units.py
+++ units.py
@@ -1,6 +1,7 @@
 import random, functools
 
-def food(*args):
+
+def food_decorator(*args):
     def food_factory(func):
         @functools.wraps(func)
         def wrapper(self, unit):
@@ -9,14 +10,29 @@ def food(*args):
         return wrapper
     return food_factory
 
-class Seaweed():
+
+class Unit:
+	
+	def hunt(self, aquarium):
+		some_fish = aquarium[random.randint(0, len(aquarium)-1)]
+		was_eat = self.eat(some_fish)
+		if was_eat:						
+			aquarium.remove(some_fish)
+		elif(self.__class__.__name__ == Fish and some_unit.__class__.__name__ == PredatorFish):
+			current_fish = self
+			some_fish.eat(current_fish)
+			aquarium.remove(current_fish)
+
+
+class Seaweed:
 
 	def __init__(self, weight):
 		if weight > 3 or weight < 1:
 			raise ValueError("Weight must be less then 4 and more than 0")
 		self.weight = weight
 
-class Fish():
+
+class Fish(Unit):
 
 	def __init__(self, weight):
 		if weight > 9 or weight < 1:
@@ -25,41 +41,37 @@ class Fish():
 		self.count_unit_eaten = 0
 
 	def hunt(self, aquarium):
-		i = random.randint(0, len(aquarium)-1)
-		some_unit = aquarium[i]
-		was_eat = self.eat(some_unit)
-		if was_eat:						# Если хищник съел юнита, то удаляем съеденное из аквариума
-			aquarium.pop(i)
+		super().hunt(aquarium)
 
-	@food(Seaweed)
+	@food_decorator(Seaweed)
 	def eat(self, some_unit, result = False):
 		if result:
 			self.weight = self.weight + some_unit.weight
 			self.count_unit_eaten += 1
 			return True
+		else: return False
+
 		
-class PredatorFish(Fish):
+class PredatorFish(Unit):
 	DEFAULT_PREDATOR_WEIGHT = 10			# this is a constant: weight, for predator fish
 
 	def __init__(self):
 		self.weight = self.DEFAULT_PREDATOR_WEIGHT
-		self.count_fish_eaten = 0                            # count of fish eaten
+		self.count_unit_eaten = 0                            # count of fish eaten
 
 	def hunt(self, aquarium):
-		i = random.randint(0, len(aquarium)-1)
-		some_fish = aquarium[i]
-		was_eat = self.eat(some_fish)
-		if was_eat:						
-			aquarium.pop(i)
+		super().hunt(aquarium)
 
-	@food(Fish)
+	@food_decorator(Fish)
 	def eat(self, some_unit, result = False):
 		if result:
 			self.weight = self.weight + some_unit.weight
-			self.count_fish_eaten += 1
+			self.count_unit_eaten += 1
 			return True
+		else: return False
 
-class Sneil():
+
+class Sneil(Unit):
 
 	def __init__(self, weight):
 		if weight > 5 or weight < 1:
@@ -68,18 +80,16 @@ class Sneil():
 		self.count_unit_eaten = 0
 
 	def hunt(self, aquarium):
-		i = random.randint(0, len(aquarium)-1)
-		some_unit = aquarium[i]
-		was_eat = self.eat(some_unit)
-		if was_eat:
-			aquarium.pop(i)
+		super().hunt(aquarium)
 
-	@food(Seaweed)
+	@food_decorator(Seaweed)
 	def eat(self, some_unit, result = False):
 		if result:
 			self.weight = self.weight + some_unit.weight
 			self.count_unit_eaten += 1
 			return True
+		else: return False
+
 
 class Aquarium:
 
